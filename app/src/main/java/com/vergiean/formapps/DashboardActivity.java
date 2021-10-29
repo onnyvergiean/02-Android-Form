c1940b6f6b8797133560637a88be1ca126f101c5
Onny Vergiea Saputra <onny.s@students.amikom.ac.id>
2021-10-29 13:53:05 +0700

    Add setText and getIntent to call intent to DashboardActivity

--- app/src/main/java/com/vergiean/formapps/DashboardActivity.java
+++ app/src/main/java/com/vergiean/formapps/DashboardActivity.java
@@ -3,6 +3,7 @@ package com.vergiean.formapps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        TextView tvName = (TextView) findViewById(R.id.idTvName);
+        TextView tvEmail = (TextView) findViewById(R.id.idTvEmail);
+        TextView tvPhone = (TextView) findViewById(R.id.idTvPhone);
+
+        tvName.setText(getIntent().getStringExtra(MainActivity.EXTRA_NAME));
+        tvEmail.setText(getIntent().getStringExtra(MainActivity.EXTRA_EMAIL));
+        tvPhone.setText(getIntent().getStringExtra(MainActivity.EXTRA_PHONE));
     }
 }

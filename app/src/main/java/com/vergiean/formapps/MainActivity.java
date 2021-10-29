e059e026e68fd7f90ad4abfd510c242bfdecfabb
Onny Vergiea Saputra <onny.s@students.amikom.ac.id>
2021-10-29 13:37:10 +0700

    Add EXTRA variabel

--- app/src/main/java/com/vergiean/formapps/MainActivity.java
+++ app/src/main/java/com/vergiean/formapps/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    final public static String EXTRA_NAME = "extra_name";
+    final public static String EXTRA_EMAIL = "extra_email";
+    final public static String EXTRA_PHONE = "extra_phone";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         Button btnSave = (Button) findViewById(R.id.idBtnSave);
         btnSave.setOnClickListener(v -> {
             Intent loginIntent = new Intent(MainActivity.this, DashboardActivity.class);
+            loginIntent.putExtra(EXTRA_NAME, inputName.getText().toString());
+            loginIntent.putExtra(EXTRA_EMAIL, inputEmail.getText().toString());
+            loginIntent.putExtra(EXTRA_PHONE, inputPhone.getText().toString());
             startActivity(loginIntent);
         });
     }
